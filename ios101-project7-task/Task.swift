bbdf0a38bfc30ef95dae91d700a19ac1da1ed156
Immanuella Umoren <immanuellaumoren@gmail.com>
2024-04-06 16:46:19 -0500

    All features completed

--- ios101-project7-task/Task.swift
+++ ios101-project7-task/Task.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 // The Task model
-struct Task {
+struct Task : Encodable, Decodable {
 
     // The task's title
     var title: String
@@ -44,26 +44,37 @@ struct Task {
 
     // The date the task was created
     // This property is set as the current date whenever the task is initially created.
-    let createdDate: Date = Date()
+    var createdDate: Date = Date()
 
     // An id (Universal Unique Identifier) used to identify a task.
-    let id: String = UUID().uuidString
+    var id: String = UUID().uuidString
 }
 
 // MARK: - Task + UserDefaults
 extension Task {
+    private static let tasksKey = "tasksKey"
 
 
     // Given an array of tasks, encodes them to data and saves to UserDefaults.
     static func save(_ tasks: [Task]) {
 
         // TODO: Save the array of tasks
+        let encoder = JSONEncoder()
+        if let encodedTasks = try? encoder.encode(tasks) {
+            UserDefaults.standard.set(encodedTasks, forKey: tasksKey)
+        }
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
         
         // TODO: Get the array of saved tasks from UserDefaults
+        if let savedTasks = UserDefaults.standard.object(forKey: tasksKey) as? Data {
+            let decoder = JSONDecoder()
+            if let loadedTasks = try? decoder.decode([Task].self, from: savedTasks) {
+                return loadedTasks
+            }
+        }
 
         return [] // 👈 replace with returned saved tasks
     }
@@ -72,5 +83,13 @@ extension Task {
     func save() {
 
         // TODO: Save the current task
+        var tasks = Task.getTasks()
+        if let index = tasks.firstIndex(where: { $0.id == self.id }) {
+            tasks.remove(at: index)
+            tasks.insert(self, at: index)
+        } else {
+            tasks.append(self)
+        }
+        Task.save(tasks)
     }
 }
